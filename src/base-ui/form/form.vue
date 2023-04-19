37ef939a5b5fcbac7ce008bdbf18fdfc419c110e
tomoYuuki <446679223@qq.com>
2023-04-19 16:38:57 +0800

    data-form预备

---
+++ src/base-ui/form/form.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="form-container">form</div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/demo/data-form.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="data-form-container">
+    <house-item-v3 :info="[{ name: 'heh', location: 'hehe' }]"></house-item-v3>
+    <form></form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
